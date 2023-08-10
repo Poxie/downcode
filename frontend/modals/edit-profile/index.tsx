9d03c57532b1534174514a2c9f44868593bf3687
Poxie <poxen12@hotmail.com>
2023-08-10 22:15:13 +0200

    Converting feedback string to boolean

--- frontend/modals/edit-profile/index.tsx
+++ frontend/modals/edit-profile/index.tsx
@@ -163,7 +163,7 @@ export const EditProfile = () => {
                     )}
                     <Button 
                         onClick={updateProfile}
-                        disabled={loading || !changesHaveBeenMade || feedback}
+                        disabled={loading || !changesHaveBeenMade || !!feedback}
                     >
                         {loading ? 'Saving changes...' : (
                             changesHaveBeenMade ? (
