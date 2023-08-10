3099ccb3bb2010a8aa167e64b6430b9f64442e2c
Poxie <poxen12@hotmail.com>
2023-08-10 17:15:23 +0200

    Added danger button type

--- frontend/components/button/index.tsx
+++ frontend/components/button/index.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 
-type ButtonType = 'primary' | 'secondary' | 'transparent';
+type ButtonType = 'primary' | 'secondary' | 'danger' | 'transparent';
 
+const typeToColor = (type: ButtonType) => {
+    if(type === 'danger') return 'text-red-500';
+    return 'text-primary'
+}
 const typeToBackground = (type: ButtonType) => {
     if(type === 'primary') return 'bg-c-primary';
-    if(type === 'secondary') return 'bg-tertiary';
+    if(type === 'secondary' || type === 'danger') return 'bg-tertiary';
     if(type === 'transparent') return 'bg-transparent';
 }
 const typeToHover = (type: ButtonType) => {
     if(type === 'primary') return 'hover:bg-c-secondary';
-    if(type === 'secondary') return 'hover:bg-quaternary';
-    if(type === 'transparent') return 'hover:bg-quaternary';
+    return 'hover:bg-quaternary';
 }
 
 export const Button: React.FC<{
@@ -21,10 +24,11 @@ export const Button: React.FC<{
     className?: string;
     disabled?: boolean;
 }> = ({ children, onClick, href, disabled, className='', type='primary' }) => {
+    const color = typeToColor(type);
     const background = typeToBackground(type);
     const backgroundHover = typeToHover(type);
 
-    className = `block ${!className.includes('py') ? 'py-[15px]' : ''} ${!className.includes('px') ? 'px-[20px]' : ''} rounded-lg text-sm transition-colors ${background} ${backgroundHover} focus:ring-1 focus:ring-offset-2 ` + className;
+    className = `block ${!className.includes('py') ? 'py-[15px]' : ''} ${!className.includes('px') ? 'px-[20px]' : ''} rounded-lg text-sm transition-colors ${color} ${background} ${backgroundHover} focus:ring-1 focus:ring-offset-2 ` + className;
     return(
         href ? (
             <Link 
