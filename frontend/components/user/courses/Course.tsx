19b8ab8375f7f1603ce901ef1690abed2cae9297
Poxie <poxen12@hotmail.com>
2023-08-12 19:49:11 +0200

    Added course skill level

--- frontend/components/user/courses/Course.tsx
+++ frontend/components/user/courses/Course.tsx
@@ -7,15 +7,21 @@ export const Course: React.FC<{
     draftId: string;
 }> = ({ draftId }) => {
     const course = useAppSelector(state => selectCourseInfo(state, draftId));
+    if(!course) return null;
+
+    const { title, description, skillLevel } = course;
 
     return(
         <div className="border-[1px] border-quaternary bg-tertiary rounded-lg mb-2">
             <div className="p-[14px]">
                 <div className="flex items-center gap-[14px]">
                     <span className="text-lg font-semibold">
-                        {course?.title || 'Course title not set'}
+                        {title || 'Course title not set'}
                     </span>
                     <div className="flex gap-1">
+                        <CourseChip>
+                            {skillLevel.slice(0,1).toUpperCase() + skillLevel.slice(1)}
+                        </CourseChip>
                         <CourseChip>
                             30 minutes
                         </CourseChip>
@@ -25,7 +31,7 @@ export const Course: React.FC<{
                     </div>
                 </div>
                 <span className="text-sm text-secondary mt-2 block">
-                    {course?.description || 'Course description not set'}
+                    {description || 'Course description not set'}
                 </span>
             </div>
             <div className="flex justify-between p-[14px] border-t-[1px] border-quaternary">
