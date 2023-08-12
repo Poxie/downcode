665a7ea3a2ab9399c970b81046e299af6d773838
Poxie <poxen12@hotmail.com>
2023-08-12 16:59:37 +0200

    Added route to update section

--- backend/src/routes/sections.ts
+++ backend/src/routes/sections.ts
@@ -2,6 +2,8 @@ import * as express from 'express';
 import { Request, Response } from 'express';
 import { Section } from '../entity/section.entity';
 import { myDataSource } from '../app-data-source';
+import { getUserIdFromHeaders } from '../utils/auth';
+import { ALLOWED_SECTION_UPDATE_PROPS } from '../constants';
 
 const router = express.Router();
 
@@ -18,6 +20,29 @@ router.get('/sections/:sectionId', async (req: Request, res: Response) => {
     if(!section) return res.status(404).send({ message: 'Section not found.' });
 
     return res.json(section);
+})
+
+router.patch('/sections/:sectionId', async (req: Request, res: Response) => {
+    const section = await getSection(req.params.sectionId);
+    if(!section) return res.status(404).send({ message: 'Section not found.' });
+    
+    // const selfId = await getUserIdFromHeaders(req.headers);
+
+    // if(selfId !== course.author.id) return res.status(401).send({ message: 'Unauthorized.' });
+
+    for(const key of Object.keys(req.body)) {
+        if(!ALLOWED_SECTION_UPDATE_PROPS.includes(key)) {
+            return res.status(400).send({ message: `${key} is an invalid property.` });
+        }
+    }
+
+    await myDataSource.getRepository(Section).update(
+        { id: section.id },
+        req.body
+    )
+    const updatedSection = await getSection(section.id);
+
+    return res.json(updatedSection);
 })
 
 export default router;
